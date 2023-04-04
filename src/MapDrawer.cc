9cd84c634bdef6c3346d91f4725390269fd83b80
Flanker-E <1014211757@qq.com>
2023-04-04 11:09:45 -0400

    Add camera pose drew

--- src/MapDrawer.cc
+++ src/MapDrawer.cc
@@ -595,38 +595,40 @@ void MapDrawer::SetCurrentCameraPose(const cv::Mat &Tcw)
 
 void MapDrawer::GetCurrentOpenGLCameraMatrix(pangolin::OpenGlMatrix &M)
 {
-    if(!mCameraPose.empty())
-    {
+//    if(!mCameraPose.empty())
+//    {
         cv::Mat Rwc(3,3,CV_32F);
         cv::Mat twc(3,1,CV_32F);
-        {
-            unique_lock<mutex> lock(mMutexCamera);
-            Rwc = mCameraPose.rowRange(0,3).colRange(0,3).t();
-            twc = -Rwc*mCameraPose.rowRange(0,3).col(3);
-        }
+//        {
+        gtsam::Pose3 pose3=s->this_step.odom;
+        gtsam::Matrix44 poseMatrix = pose3.matrix();
+//        unique_lock<mutex> lock(mMutexCamera);
+//        Rwc = mCameraPose.rowRange(0,3).colRange(0,3).t();
+//        twc = -Rwc*mCameraPose.rowRange(0,3).col(3);
+//        }
 
-        M.m[0] = Rwc.at<float>(0,0);
-        M.m[1] = Rwc.at<float>(1,0);
-        M.m[2] = Rwc.at<float>(2,0);
+        M.m[0] = poseMatrix(0,0);//Rwc.at<float>(0,0);
+        M.m[1] = poseMatrix(1,0);//Rwc.at<float>(1,0);
+        M.m[2] = poseMatrix(2,0);//Rwc.at<float>(2,0);
         M.m[3]  = 0.0;
 
-        M.m[4] = Rwc.at<float>(0,1);
-        M.m[5] = Rwc.at<float>(1,1);
-        M.m[6] = Rwc.at<float>(2,1);
+        M.m[4] = poseMatrix(0,1);//Rwc.at<float>(0,1);
+        M.m[5] = poseMatrix(1,1);//Rwc.at<float>(1,1);
+        M.m[6] = poseMatrix(2,1);//Rwc.at<float>(2,1);
         M.m[7]  = 0.0;
 
-        M.m[8] = Rwc.at<float>(0,2);
-        M.m[9] = Rwc.at<float>(1,2);
-        M.m[10] = Rwc.at<float>(2,2);
+        M.m[8] = poseMatrix(0,2);//Rwc.at<float>(0,2);
+        M.m[9] = poseMatrix(1,2);//Rwc.at<float>(1,2);
+        M.m[10] = poseMatrix(2,2);//Rwc.at<float>(2,2);
         M.m[11]  = 0.0;
 
-        M.m[12] = twc.at<float>(0);
-        M.m[13] = twc.at<float>(1);
-        M.m[14] = twc.at<float>(2);
+        M.m[12] = poseMatrix(0,3);//twc.at<float>(0);
+        M.m[13] = poseMatrix(1,3);//twc.at<float>(1);
+        M.m[14] = poseMatrix(2,3);//twc.at<float>(2);
         M.m[15]  = 1.0;
-    }
-    else
-        M.SetIdentity();
+//    }
+//    else
+//        M.SetIdentity();
 }
 
 } //namespace ORB_SLAM
