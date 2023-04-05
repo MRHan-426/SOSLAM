e4f4d4b0c72fd07869dba05d7d7c06e74477bbc4
MRHan-426 <1207140677@qq.com>
2023-04-04 23:35:45 -0400

    add HandMadeData.cpp

--- include/HandMadeData.h
+++ include/HandMadeData.h
@@ -37,155 +37,31 @@ public:
     void restart() override {
         i = 0;
     }
-    HandMadeData(const std::string& img_path = "../input/img/", const std::string& xml_path = "../input/xml/", const std::string& calib_file= "../input/calib_params.txt", const std::string& odom_file= "../input/odom.txt")
-    : img_path_(img_path),xml_path_(xml_path),calib_file_(calib_file) {
-        i = 0;
-        if (file_num(img_path) == 0 || file_num(xml_path) == 0){
-            std::cerr << "WARN: there be no imgs or xmls in the folder path, please check." << std::endl;
-        }
-        else if (file_num(img_path) != file_num(xml_path)){
-            std::cerr << "WARN: number of images and xmls does not match, please check." << std::endl;
-        }
-        else{
-            num_img_ = file_num(img_path);
-        }
+    explicit HandMadeData(const std::string& img_path = "../input/img/", const std::string& xml_path = "../input/xml/", const std::string& calib_file= "../input/calib_params.txt", const std::string& odom_file= "../input/odom.txt");
 
-        std::ifstream infile(odom_file);
+    int file_num(const std::string& folder_path);
 
-        if (infile.is_open()) {
-            std::string line;
-            while (std::getline(infile, line)) {
-                odom_data_.push_back(line);
-            }
-            infile.close();
-            if (odom_data_.size() != static_cast<size_t>(num_img_)){
-                std::cerr << "length of image and odom does not match, please check." << std::endl;
-            }
-        } else {
-            std::cerr << "WARN: cannot open odom.txt, please check." << std::endl;
-        }
-
-    }
-
-    int file_num(const std::string& folder_path){
-        DIR *dir;
-        struct dirent *ent;
-        struct stat buf;
-        int file_count = 0;
-        if ((dir = opendir(folder_path.c_str())) != NULL) {
-            while ((ent = readdir(dir)) != NULL) {
-                std::string full_path = folder_path + "/" + ent->d_name;
-                if (stat(full_path.c_str(), &buf) == 0 && S_ISREG(buf.st_mode)) {
-                    file_count++;
-                }
-            }
-            closedir(dir);
-            return file_count;
-        }
-        else{
-            return int(0);
-        }
-    }
-
-    gtsam::Vector5 calib_rgb() override {
-
-        try {
-            std::ifstream infile(calib_file_);
-            if (!infile.is_open()) {
-                throw std::runtime_error("Failed to open file!");
-            }
-            std::string line;
-            gtsam::Vector5 result;
-            // Just read the first line
-            getline(infile, line);
-            std::istringstream iss(line);
-            double num;
-            for (int j = 0; j < 5 && iss >> num; j++) {
-                result(j) = num;
-            }
-            infile.close();
-            return result;
-        }
-        catch (std::exception& e) {
-            std::cout << "Use backup calibration parameters" << std::endl;
-            gtsam::Vector5 result;
-            result << 525.0, 525.0, 0.0, 160.0, 120.0;
-            return result;
-        }
-    }
+    gtsam::Vector5 calib_rgb() override;
 
     bool done() const override {
         return i == num_img_;
     }
 
-    std::tuple<gtsam::Pose3, gtsam::Matrix3 , cv::Mat> next(SoSlamState& state) override
-    {
-        std::vector<std::string> parts;
-        std::stringstream ss(odom_data_[i]); //start from 0
-        std::string token;
-        while (ss >> token) {
-            parts.push_back(token);
-        }
-        auto tx = std::stod(parts[1]);
-        auto ty = std::stod(parts[2]);
-        auto tz = std::stod(parts[3]);
-        auto qx = std::stod(parts[4]);
-        auto qy = std::stod(parts[5]);
-        auto qz = std::stod(parts[6]);
-        auto qw = std::stod(parts[7]);
-        gtsam::Pose3 pose(gtsam::Rot3::Quaternion(qw, qx, qy, qz), gtsam::Point3(tx, ty, tz));
-
-        ++i;
-
-        std::string img_name = img_path_ + std::to_string(i) + ".png"; //start from 1.png
-
-        gtsam::Matrix3 mat = gtsam::Matrix3::Zero();
-
-        cv::Mat img = cv::imread(img_name, cv::IMREAD_COLOR); //BGR
-
-        return std::make_tuple(pose, mat, img);
-    }
+    std::tuple<gtsam::Pose3, gtsam::Matrix3 , cv::Mat> next(SoSlamState& state) override;
 
 private:
     int i;
 };
 
+
 class HandMadeDetector : public BaseDetector
 {
 public:
-    HandMadeDetector() : i(0) {}
+    std::string xml_path_;
 
-    std::vector<Detection> detect(SoSlamState& state) override {
-        ++i;
-        std::vector<Detection> detections;
+    explicit HandMadeDetector(const std::string& xml_path = "../input/xml/") : xml_path_(xml_path), i(0){}
 
-        std::string xml_name = "../input/xml/" + std::to_string(i) + ".xml"; //start from 1.xml
-
-        pugi::xml_document doc;
-        if (!doc.load_file(xml_name.c_str())) {
-            std::cerr << "WARN: cannot open xml file, please check." << std::endl;
-        }
-        pugi::xml_node root = doc.child("annotation");
-
-        for (pugi::xml_node object = root.child("object"); object; object = object.next_sibling("object")) {
-
-            pugi::xml_node bndbox = object.child("bndbox");
-            pugi::xml_node name = object.child("name");
-            pugi::xml_node objectkey = object.child("objectkey");
-
-            auto key = std::stoull(objectkey.text().get());
-            gtsam::Key quadrickey = gtsam::Symbol('q', key);
-
-            std::string label = name.text().get();
-            gtsam::Vector4 bounds;
-            bounds << bndbox.child("xmin").text().as_int(), bndbox.child("ymin").text().as_int(), bndbox.child("xmax").text().as_int(), bndbox.child("ymax").text().as_int();
-
-            detections.emplace_back(
-                    label, bounds, state.this_step.pose_key, quadrickey
-            );
-        }
-        return detections;
-    }
+    std::vector<Detection> detect(SoSlamState& state) override;
 
 private:
     int i;
@@ -197,32 +73,10 @@ class HandMadeAssociator : public BaseAssociator
 public:
     HandMadeAssociator() = default;
 
-    static std::vector<Detection> flat(const std::vector<std::vector<Detection>>& list_of_lists) {
-        std::vector<Detection> result;
-        for (const auto& sublist : list_of_lists) {
-            result.insert(result.end(), sublist.begin(), sublist.end());
-        }
-        return result;
-    }
+    static std::vector<Detection> flat(const std::vector<std::vector<Detection>>& list_of_lists);
 
-    std::tuple<std::vector<Detection>, std::vector<Detection>, std::vector<Detection>> associate(SoSlamState& state) override
-    {
-        // Our dataset has been associated by hand
-        // In fact the associator here is fake
-        auto& s = state;
-        auto& n = state.this_step;
-        std::vector<Detection> new_ds = (!n.isValid()) ? std::vector<Detection>{} : n.detections;
-        std::vector<Detection> newly_associated = new_ds;
+    std::tuple<std::vector<Detection>, std::vector<Detection>, std::vector<Detection>> associate(SoSlamState& state) override;
 
-        std::vector<Detection> associated = s.associated_;
-        associated.insert(associated.end(), newly_associated.begin(), newly_associated.end());
-
-        // In fact, we make sure there are no unassocaited objects in the dataset.
-        std::vector<Detection> unassociated;
-        unassociated = s.unassociated_;
-
-        return std::make_tuple(newly_associated, associated, unassociated);
-    }
 };
 } // namespace gtsam_soslam
 
